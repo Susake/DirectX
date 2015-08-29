ce07b92d89409bcd12683ba7b3aa75e05ee878ea
Susake <2753171136@qq.com>
2015-08-29 11:30:21 +0800

    Create model.cpp

---
+++ model.cpp
@@ -0,0 +1,373 @@
+#include "model.h"
+
+HRESULT AllocateName(LPCSTR Name, LPSTR* pNewName)
+{
+	UINT cbLength;
+
+	if (Name != NULL)
+	{
+		cbLength = (UINT)strlen(Name) + 1;
+		*pNewName = new CHAR[cbLength];
+		memcpy(*pNewName, Name, cbLength * sizeof(CHAR));
+	}
+	else
+	{
+		*pNewName = NULL;
+	}
+
+	return S_OK;
+}
+
+HRESULT GenerateSkinnedMesh(IDirect3DDevice9* pd3dDevice, D3DXMESHCONTAINER_DERIVED* pMeshContainer)
+{
+	D3DCAPS9 d3dCaps;
+	pd3dDevice->GetDeviceCaps(&d3dCaps);
+
+	if (pMeshContainer->pSkinInfo == NULL)
+		return S_OK;
+
+	SAFE_RELEASE(pMeshContainer->MeshData.pMesh);
+	SAFE_RELEASE(pMeshContainer->pBoneCombinationBuf);
+
+
+	if (FAILED(pMeshContainer->pSkinInfo->ConvertToBlendedMesh(
+		pMeshContainer->pOrigMesh,
+		D3DXMESH_MANAGED | D3DXMESHOPT_VERTEXCACHE,
+		pMeshContainer->pAdjacency,
+		NULL, NULL, NULL,
+		&pMeshContainer->NumInfl,
+		&pMeshContainer->NumAttributeGroups,
+		&pMeshContainer->pBoneCombinationBuf,
+		&pMeshContainer->MeshData.pMesh)))
+		return E_FAIL;
+	return S_OK;
+}
+
+HRESULT Model::CreateFrame(LPCSTR Name, LPD3DXFRAME* ppNewFrame)
+{
+	HRESULT hr = S_OK;
+	D3DXFRAME_DERIVED *pFrame;
+
+	*ppNewFrame = NULL;
+
+	pFrame = new D3DXFRAME_DERIVED;
+	if (FAILED(AllocateName(Name, (LPSTR*)&pFrame->Name)))
+	{
+		delete pFrame;
+		return hr;
+	}
+
+	D3DXMatrixIdentity(&pFrame->TransformationMatrix);
+	D3DXMatrixIdentity(&pFrame->CombinedTransformationMatrix);
+
+	pFrame->pMeshContainer = NULL;
+	pFrame->pFrameSibling = NULL;
+	pFrame->pFrameFirstChild = NULL;
+
+	*ppNewFrame = pFrame;
+	pFrame = NULL;
+
+	return hr;
+}
+
+HRESULT Model::CreateMeshContainer(LPCSTR Name,
+	CONST D3DXMESHDATA *pMeshData,
+	CONST D3DXMATERIAL *pMaterials,
+	CONST D3DXEFFECTINSTANCE *pEffectInstances,
+	DWORD NumMaterials,
+	CONST DWORD *pAdjacency,
+	LPD3DXSKININFO pSkinInfo,
+	LPD3DXMESHCONTAINER *ppNewMeshContainer)
+{
+	HRESULT hr;
+	UINT NumFaces;
+	UINT iMaterial;
+	UINT iBone, cBones;
+	LPDIRECT3DDEVICE9 pd3dDevice = NULL;
+	D3DXMESHCONTAINER_DERIVED *pMeshContainer = NULL;
+
+	LPD3DXMESH pMesh = NULL;
+
+	*ppNewMeshContainer = NULL;
+
+	if (pMeshData->Type != D3DXMESHTYPE_MESH)
+	{
+		hr = E_FAIL;
+		goto e_Exit;
+	}
+
+	pMesh = pMeshData->pMesh;
+
+	if (pMesh->GetFVF() == 0)
+	{
+		hr = E_FAIL;
+		goto e_Exit;
+	}
+
+	pMeshContainer = new D3DXMESHCONTAINER_DERIVED;
+	memset(pMeshContainer, 0, sizeof(D3DXMESHCONTAINER_DERIVED));
+
+	hr = AllocateName(Name, &pMeshContainer->Name);
+	if (FAILED(hr))
+		goto e_Exit;
+
+	pMesh->GetDevice(&pd3dDevice);
+	NumFaces = pMesh->GetNumFaces();
+
+	if (!(pMesh->GetFVF() & D3DFVF_NORMAL))
+	{
+		pMeshContainer->MeshData.Type = D3DXMESHTYPE_MESH;
+
+		hr = pMesh->CloneMeshFVF(pMesh->GetOptions(), pMesh->GetFVF() | D3DFVF_NORMAL,
+			pd3dDevice, &pMeshContainer->MeshData.pMesh);
+		if (FAILED(hr))
+			goto e_Exit;
+
+		pMesh = pMeshContainer->MeshData.pMesh;
+
+		D3DXComputeNormals(pMesh, NULL);
+	}
+	else
+	{
+		pMeshContainer->MeshData.pMesh = pMesh;
+		pMeshContainer->MeshData.Type = D3DXMESHTYPE_MESH;
+
+		pMesh->AddRef();
+	}
+
+	pMeshContainer->NumMaterials = max(1, NumMaterials);
+	pMeshContainer->pMaterials = new D3DXMATERIAL[pMeshContainer->NumMaterials];
+	pMeshContainer->ppTextures = new LPDIRECT3DTEXTURE9[pMeshContainer->NumMaterials];
+	pMeshContainer->pAdjacency = new DWORD[NumFaces * 3];
+
+	memcpy(pMeshContainer->pAdjacency, pAdjacency, sizeof(DWORD)* NumFaces * 3);
+	memset(pMeshContainer->ppTextures, 0, sizeof(LPDIRECT3DTEXTURE9)* pMeshContainer->NumMaterials);
+
+	if (NumMaterials > 0)
+	{
+		memcpy(pMeshContainer->pMaterials, pMaterials, sizeof(D3DXMATERIAL)* NumMaterials);
+
+		for (iMaterial = 0; iMaterial < NumMaterials; iMaterial++)
+		{
+			if (pMeshContainer->pMaterials[iMaterial].pTextureFilename != NULL)
+			{
+				if (FAILED(D3DXCreateTextureFromFileA(pd3dDevice, pMeshContainer->pMaterials[iMaterial].pTextureFilename,
+					&pMeshContainer->ppTextures[iMaterial])))
+					pMeshContainer->ppTextures[iMaterial] = NULL;
+
+				pMeshContainer->pMaterials[iMaterial].pTextureFilename = NULL;
+			}
+		}
+	}
+	else
+	{
+		pMeshContainer->pMaterials[0].pTextureFilename = NULL;
+		memset(&pMeshContainer->pMaterials[0].MatD3D, 0, sizeof(D3DMATERIAL9));
+		pMeshContainer->pMaterials[0].MatD3D.Diffuse.r = 0.5f;
+		pMeshContainer->pMaterials[0].MatD3D.Diffuse.g = 0.5f;
+		pMeshContainer->pMaterials[0].MatD3D.Diffuse.b = 0.5f;
+		pMeshContainer->pMaterials[0].MatD3D.Specular = pMeshContainer->pMaterials[0].MatD3D.Diffuse;
+	}
+
+	if (pSkinInfo != NULL)
+	{
+		pMeshContainer->pSkinInfo = pSkinInfo;
+		pSkinInfo->AddRef();
+
+		pMeshContainer->pOrigMesh = pMesh;
+		pMesh->AddRef();
+
+		cBones = pSkinInfo->GetNumBones();
+		pMeshContainer->pBoneOffsetMatrices = new D3DXMATRIX[cBones];
+
+		for (iBone = 0; iBone < cBones; iBone++)
+		{
+			pMeshContainer->pBoneOffsetMatrices[iBone] = *(pMeshContainer->pSkinInfo->GetBoneOffsetMatrix(iBone));
+		}
+
+		hr = GenerateSkinnedMesh(pd3dDevice, pMeshContainer);
+		if (FAILED(hr))
+			goto e_Exit;
+	}
+
+	*ppNewMeshContainer = pMeshContainer;
+	pMeshContainer = NULL;
+
+e_Exit:
+	SAFE_RELEASE(pd3dDevice);
+
+	if (pMeshContainer != NULL)
+	{
+		DestroyMeshContainer(pMeshContainer);
+	}
+
+	return hr;
+}
+
+HRESULT Model::DestroyFrame(LPD3DXFRAME pFrameToFree)
+{
+	SAFE_DELETE_ARRAY(pFrameToFree->Name);
+	SAFE_DELETE(pFrameToFree);
+	return S_OK;
+}
+
+HRESULT Model::DestroyMeshContainer(LPD3DXMESHCONTAINER pMeshContainerBase)
+{
+	UINT iMaterial;
+	D3DXMESHCONTAINER_DERIVED* pMeshContainer = (D3DXMESHCONTAINER_DERIVED*)pMeshContainerBase;
+
+	SAFE_DELETE_ARRAY(pMeshContainer->Name);
+	SAFE_DELETE_ARRAY(pMeshContainer->pAdjacency);
+	SAFE_DELETE_ARRAY(pMeshContainer->pMaterials);
+	SAFE_DELETE_ARRAY(pMeshContainer->pBoneOffsetMatrices);
+
+	if (pMeshContainer->ppTextures != NULL)
+	{
+		for (iMaterial = 0; iMaterial < pMeshContainer->NumMaterials; iMaterial++)
+		{
+			SAFE_RELEASE(pMeshContainer->ppTextures[iMaterial]);
+		}
+	}
+
+	SAFE_DELETE_ARRAY(pMeshContainer->ppTextures);
+	SAFE_DELETE_ARRAY(pMeshContainer->ppBoneMatrixPtrs);
+	SAFE_RELEASE(pMeshContainer->pBoneCombinationBuf);
+	SAFE_RELEASE(pMeshContainer->MeshData.pMesh);
+	SAFE_RELEASE(pMeshContainer->pSkinInfo);
+	SAFE_RELEASE(pMeshContainer->pOrigMesh);
+	SAFE_DELETE(pMeshContainer);
+	return S_OK;
+}
+
+HRESULT SetupBoneMatrixPointers(LPD3DXFRAME pFrameBase, LPD3DXFRAME pFrameRoot)
+{
+	if (pFrameBase->pMeshContainer != NULL)
+	{
+		D3DXFRAME_DERIVED* pFrame = NULL;
+		D3DXMESHCONTAINER_DERIVED* pMeshContainer = (D3DXMESHCONTAINER_DERIVED*)pFrameBase->pMeshContainer;
+		//LPD3DXMESH m_pOrigMesh = pMeshContainer->MeshData.pMesh;///
+		if (pMeshContainer->pSkinInfo != NULL)
+		{
+			UINT cBones = pMeshContainer->pSkinInfo->GetNumBones();
+			pMeshContainer->ppBoneMatrixPtrs = new D3DXMATRIX*[cBones];
+			for (UINT iBone = 0; iBone < cBones; iBone++)
+			{
+				pFrame = (D3DXFRAME_DERIVED*)D3DXFrameFind(pFrameRoot, pMeshContainer->pSkinInfo->GetBoneName(iBone));
+				if (pFrame == NULL) return E_FAIL;
+
+				pMeshContainer->ppBoneMatrixPtrs[iBone] = &pFrame->CombinedTransformationMatrix;
+			}
+		}
+	}
+
+	if (pFrameBase->pFrameSibling != NULL)
+	{
+		if (FAILED(SetupBoneMatrixPointers(pFrameBase->pFrameSibling, pFrameRoot)))
+			return E_FAIL;
+	}
+
+	if (pFrameBase->pFrameFirstChild != NULL)
+	{
+		if (FAILED(SetupBoneMatrixPointers(pFrameBase->pFrameFirstChild, pFrameRoot)))
+			return E_FAIL;
+	}
+
+	return S_OK;
+}
+
+void DrawMeshContainer(IDirect3DDevice9* pd3dDevice, LPD3DXMESHCONTAINER pMeshContainerBase, LPD3DXFRAME pFrameBase)
+{
+	D3DXMESHCONTAINER_DERIVED* pMeshContainer = (D3DXMESHCONTAINER_DERIVED*)pMeshContainerBase;
+	D3DXFRAME_DERIVED* pFrame = (D3DXFRAME_DERIVED*)pFrameBase;
+	UINT iMaterial;
+	UINT NumBlend;
+	UINT iAttrib;
+	DWORD AttribIdPrev;
+	LPD3DXBONECOMBINATION pBoneComb;
+
+	UINT iMatrixIndex;
+	D3DXMATRIXA16 matTemp;
+	D3DCAPS9 d3dCaps;
+	pd3dDevice->GetDeviceCaps(&d3dCaps);
+
+	if (pMeshContainer->pSkinInfo != NULL)
+	{
+		AttribIdPrev = UNUSED32;
+		pBoneComb = reinterpret_cast<LPD3DXBONECOMBINATION>(pMeshContainer->pBoneCombinationBuf->GetBufferPointer());
+
+		for (iAttrib = 0; iAttrib < pMeshContainer->NumAttributeGroups; iAttrib++)
+		{
+			NumBlend = 0;
+			for (DWORD i = 0; i < pMeshContainer->NumInfl; ++i)
+			{
+				if (pBoneComb[iAttrib].BoneId[i] != UINT_MAX)
+				{
+					NumBlend = i;
+				}
+			}
+
+			if (d3dCaps.MaxVertexBlendMatrices >= NumBlend + 1)
+			{
+				for (DWORD i = 0; i < pMeshContainer->NumInfl; ++i)
+				{
+					iMatrixIndex = pBoneComb[iAttrib].BoneId[i];
+					if (iMatrixIndex != UINT_MAX)
+					{
+						D3DXMatrixMultiply(&matTemp, &pMeshContainer->pBoneOffsetMatrices[iMatrixIndex],
+							pMeshContainer->ppBoneMatrixPtrs[iMatrixIndex]);
+						pd3dDevice->SetTransform(D3DTS_WORLDMATRIX(i), &matTemp);
+					}
+				}
+
+				pd3dDevice->SetRenderState(D3DRS_VERTEXBLEND, NumBlend);
+
+				if ((AttribIdPrev != pBoneComb[iAttrib].AttribId) || (AttribIdPrev == UNUSED32))
+				{
+					pd3dDevice->SetMaterial(&pMeshContainer->pMaterials[pBoneComb[iAttrib].AttribId].MatD3D);
+					pd3dDevice->SetTexture(0, pMeshContainer->ppTextures[pBoneComb[iAttrib].AttribId]);
+					AttribIdPrev = pBoneComb[iAttrib].AttribId;
+				}
+
+				pMeshContainer->MeshData.pMesh->DrawSubset(iAttrib);
+			}
+		}
+		pd3dDevice->SetRenderState(D3DRS_VERTEXBLEND, 0);
+	}
+	else 
+	{
+		pd3dDevice->SetTransform(D3DTS_WORLD, &pFrame->CombinedTransformationMatrix);
+
+		for (iMaterial = 0; iMaterial < pMeshContainer->NumMaterials; iMaterial++)
+		{
+			pd3dDevice->SetMaterial(&pMeshContainer->pMaterials[iMaterial].MatD3D);
+			pd3dDevice->SetTexture(0, pMeshContainer->ppTextures[iMaterial]);
+			pMeshContainer->MeshData.pMesh->DrawSubset(iMaterial);
+		}
+	}
+}
+
+void DrawFrame(IDirect3DDevice9* pd3dDevice, LPD3DXFRAME pFrame)
+{
+	if (pFrame == NULL) return;
+	LPD3DXMESHCONTAINER pMeshContainer;
+	pMeshContainer = pFrame->pMeshContainer;
+	while (pMeshContainer != NULL)
+	{
+		DrawMeshContainer(pd3dDevice, pMeshContainer, pFrame);
+		pMeshContainer = pMeshContainer->pNextMeshContainer;
+	}
+
+	DrawFrame(pd3dDevice, pFrame->pFrameSibling);
+	DrawFrame(pd3dDevice, pFrame->pFrameFirstChild);
+}
+
+void UpdateFrameMatrices(LPD3DXFRAME pFrameBase, LPD3DXMATRIX pParentMatrix)
+{
+	if (pFrameBase == NULL || pParentMatrix == NULL) return;
+	D3DXFRAME_DERIVED* pFrame = (D3DXFRAME_DERIVED*)pFrameBase;
+
+	D3DXMatrixMultiply(&pFrame->CombinedTransformationMatrix, &pFrame->TransformationMatrix, pParentMatrix);
+
+	UpdateFrameMatrices(pFrame->pFrameSibling, pParentMatrix);
+	UpdateFrameMatrices(pFrame->pFrameFirstChild, &pFrame->CombinedTransformationMatrix);
+}
